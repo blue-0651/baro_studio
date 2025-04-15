667b3e6049990578fdb4f72452b44df957fbefc2
yuseonyoung <86030599+areum1121@users.noreply.github.com>
2025-04-16 00:53:35 +0900

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -73,7 +73,6 @@ export const authOptions = {
     signIn: "/login",
   },
   secret: process.env.NEXTAUTH_SECRET,
-  // debug: process.env.NODE_ENV === 'development', // Uncomment for development debugging
 };
 
 const handler = NextAuth(authOptions);
