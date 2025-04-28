7fae0dab4167b85dc66a54dee494130d8782a435
yhkim <yk110796@naver.com>
2025-04-28 17:10:00 +0900

    아웃룩 최대 업로드 용량 35mb로 수정

--- src/components/quote/quote.tsx
+++ src/components/quote/quote.tsx
@@ -54,7 +54,7 @@ type Service = {
     name: TranslationEntry;
     icon: IconDefinition;
 }
-const MAX_FILE_SIZE_MB = 50;
+const MAX_FILE_SIZE_MB = 35;
 const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
 export default function QuoteForm() {
     const { lang } = useLang() as { lang: Language };
